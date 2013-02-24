b0eb2f403c86b5a08583be6219688be6927e1826
Thilo Schwarz <thilo.schwarz@googlemail.com>
2013-02-24 18:45:11 +0100

    .

---
+++ src/test/de/thischwa/pmcms/server/StartJetty.java
@@ -0,0 +1,17 @@
+package de.thischwa.pmcms.server;
+
+import de.thischwa.pmcms.Constants;
+import de.thischwa.pmcms.configuration.BasicConfigurator;
+import de.thischwa.pmcms.configuration.InitializationManager;
+
+public class StartJetty {
+
+	public static void main(String[] args) throws Exception {
+
+		InitializationManager.start(new BasicConfigurator(Constants.APPLICATION_DIR), true);
+		
+		while(true)
+			Thread.currentThread().sleep(500);
+	}
+
+}
